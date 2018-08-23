79d6f7ff90c44076150aba5ccd2dd0b568533a51
frankmdn7 <42079512+frankmdn7@users.noreply.github.com>
2018-08-23 13:31:12 +0700

    my Summary of JS with Array

---
+++ Basic Data Structures/Array Summary.js
@@ -0,0 +1,14 @@
+
+let greetings = ['whats up?', 'hello', 'see ya!'];
+// remove last item of array, now equals ['whats up?', 'hello']
+greetings.pop(); 
+
+//  remove first item of array now equals ['hello']
+greetings.shift(); 
+
+//  push a item after last item of array, now equals ['hello','Frank'] , return new-length
+greetings.push('Frank'); 
+
+//  unshift a item before first item of array, now equals ['say','hello','Frank'], return new-length
+greetings.unshift('say'); 
+
