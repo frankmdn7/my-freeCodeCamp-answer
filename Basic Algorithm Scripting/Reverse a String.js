fbba7d60dae7ff4457d5f00fb78532b3ce6d1e0a
frankmdn7 <42079512+frankmdn7@users.noreply.github.com>
2018-08-24 13:11:16 +0700

    Update Reverse a String.js

--- Basic Algorithm Scripting/Reverse a String.js
+++ Basic Algorithm Scripting/Reverse a String.js
@@ -1,9 +1,8 @@
 function reverseString(str) {
-let arr=[];
-for (let i=0; i<str.length; i++) {
-  arr[i]=str[str.length-i-1];
-}
-return arr.join('');
+  let str2="";
+  for (let i=0; i<str.length; i++) {
+    str2=str2+str[str.length-i-1];
+  }
+  return str2;
 }
 reverseString("hello");
-
